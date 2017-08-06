89b139bcf3305cd2e742860c583106194e2ed5d4
Mio Bambino <s59330@beuth-hochschule.de>
2017-08-06 21:19:16 +0200

    how the fuck did we win?

--- Assets/VRHackaton/Scripts/TestMirror.cs
+++ Assets/VRHackaton/Scripts/TestMirror.cs
@@ -12,15 +12,35 @@ public class TestMirror : MonoBehaviour {
     public Transform mirrorLeftHand;
     public Transform mirrorRightHand;
 
+	public bool invertX, invertY, invertZ;
+
     // Use this for initialization
     void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-        mirrorHead.position = new Vector3(head.position.x, head.position.y, -head.position.z);
-        mirrorLeftHand.position = new Vector3(leftHand.position.x, leftHand.position.y, -leftHand.position.z);
-        mirrorRightHand.position = new Vector3(rightHand.position.x, rightHand.position.y, -rightHand.position.z);
-    }
+	void Update()
+	{
+
+		MirrorPosition(mirrorHead, head);
+		MirrorPosition(mirrorLeftHand, leftHand);
+		MirrorPosition(mirrorRightHand, rightHand);
+		//mirrorHead.position = new Vector3(head.position.x, head.position.y, -head.position.z);
+		//mirrorLeftHand.position =
+		//	new Vector3(leftHand.position.x, leftHand.position.y, -leftHand.position.z);
+		//mirrorRightHand.position =
+		//	new Vector3(
+		//		invertX ? -rightHand.position.x : rightHand.position.x,
+		//		invertY ? -rightHand.position.y + 2 : rightHand.position.y,
+		//		invertZ ? -rightHand.position.z : rightHand.position.z);
+	}
+
+	private void MirrorPosition(Transform mirror, Transform t)
+	{
+		mirror.position = new Vector3(
+				invertX ? -t.position.x : t.position.x,
+				invertY && t != head ? -t.position.y + 2 : t.position.y,
+				invertZ ? -t.position.z : t.position.z + 2.5f);
+	}
 }
