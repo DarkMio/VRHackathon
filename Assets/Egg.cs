aa885c4aaeadc83ba671ee447036dadd6dfcae7b
Khaled <khaled.reguieg@artcom.de>
2017-08-06 16:33:11 +0200

    [ADD] Level 2.2 entered with throwable interface.

---
+++ Assets/Egg.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Egg : ThrowableObject {
+    private Vector3 originalPos;
+	public GameObject brokenEgg;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+	void Awake() {
+		originalPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if(col.GetComponent<MirrorDudeController>() == null) {
+			if(col.transform.gameObject.name == "Bratpfanne") {
+				Debug.Log("WIIIIIN!!!");
+			}
+			else
+				Instantiate(brokenEgg, transform.position, Quaternion.identity, startParent);
+				transform.position = originalPos;
+		}
+	}
+}
---
+++ Assets/VRHackaton/Scripts/ThrowableObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableObject : MonoBehaviour, IInteractable {
+    protected Transform startParent;
+
+    public void Grab(bool value, MirrorDudeController controller)
+    {
+        if(value) {
+			gameObject.transform.parent = controller.transform;
+			gameObject.transform.position = controller.transform.position;
+		} else {
+			gameObject.transform.parent = startParent;
+			GetComponent<Rigidbody>().AddForce(GetComponent<Rigidbody>().velocity, ForceMode.Impulse);
+		}
+    }
+
+    public void Press(bool value, MirrorDudeController controller)
+    {
+        throw new NotImplementedException();
+    }
+
+	void Awake() {
+		startParent = transform.parent;
+	}
+}
